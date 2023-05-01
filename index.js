0bd9343c6f83242ff01366e4dff6cf0c97a2920c
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-01 20:29:59 +0600

    [ADD]-Create-news-and-category-data-loading-API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 5000; // define the port you want to use
 
 const categories = require("./datas/categories.json");
+const news = require("./datas/news.json");
 
 app.use(cors());
 
@@ -14,6 +15,26 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
 });
 
 app.listen(port, () => {
